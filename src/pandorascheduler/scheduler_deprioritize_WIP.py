3304e8362fa5f942ab5190fb30ce76fa0c4e13ad
Veselin Kostov <veselin.b.kostov@gmail.com>
2025-05-05 13:05:08 -0400

    а

--- src/pandorascheduler/scheduler_deprioritize_WIP.py
+++ src/pandorascheduler/scheduler_deprioritize_WIP.py
@@ -1229,9 +1229,9 @@ if __name__ == "__main__":
     # Specify observing parameters
     obs_window = timedelta(hours=24.0)
     pandora_start = "2025-10-15 00:00:00"#"2025-09-01 00:00:00"
-    pandora_stop = "2026-04-15 00:00:00"#"2026-10-01 00:00:00"
+    pandora_stop = "2026-10-15 00:00:00"#"2026-10-01 00:00:00"
     sched_start= "2025-10-15 00:00:00"#"2025-09-01 00:00:00"
-    sched_stop= "2026-04-15 00:00:00"#"2026-10-01 00:00:00"
+    sched_stop= "2026-10-15 00:00:00"#"2026-10-01 00:00:00"
 
     commissioning_time_ = 0 # days
 
