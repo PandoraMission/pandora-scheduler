670905654aad383f42657b02416a203ec347539b
Veselin Kostov <veselin.b.kostov@gmail.com>
2025-10-23 08:14:02 -0400

    a

--- src/pandorascheduler/confirm_XML_visibility.py
+++ src/pandorascheduler/confirm_XML_visibility.py
@@ -66,11 +66,18 @@ def create_visit_figure(visit_data, visit_id, target):
         visible_ = np.asarray(data['visibility']) == 1.
         times = np.asarray(data['times'])
         y_value = target_to_y[data['target']]
+        priority = data['priority']
+        if priority == '0':
+            color_ = 'gray'
+        elif priority == '1':
+            color_ = 'r'
+        elif priority == '2':
+            color_ = 'orange'
 
         vis_times = data['times']
         vis_values = [i+1 if v else None for v in data['visibility']]
         # ax.scatter(vis_times, [y_value] * len(vis_times), color='green', marker='.', s=2)
-        ax.plot(times, [y_value] * len(times), color='green', linewidth=1)
+        ax.plot(times, [y_value] * len(times), color = color_, linewidth=1)
 
         if not(all(visible_)):
             for t in times[~visible_]:
@@ -160,6 +167,8 @@ for visit in tqdm.tqdm(root.findall('ns:Visit', namespace)):
     for obs_seq in visit.findall('ns:Observation_Sequence', namespace):
         target_elem = obs_seq.find('./ns:Observational_Parameters/ns:Target', namespace)
         target = target_elem.text if target_elem is not None and target_elem.text else "No target"
+
+        priority = obs_seq.find('./ns:Observational_Parameters/ns:Priority', namespace).text
         
         start_elem = obs_seq.find('./ns:Observational_Parameters/ns:Timing/ns:Start', namespace)
         stop_elem = obs_seq.find('./ns:Observational_Parameters/ns:Timing/ns:Stop', namespace)
@@ -182,6 +191,7 @@ for visit in tqdm.tqdm(root.findall('ns:Visit', namespace)):
         
         visit_data.append({
             'target': target,
+            'priority': priority, 
             'start': datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ"),
             'stop': datetime.strptime(stop_time, "%Y-%m-%dT%H:%M:%SZ"),
             'visibility': visibility,
